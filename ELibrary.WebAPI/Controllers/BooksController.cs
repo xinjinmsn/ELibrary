9327101ff999a9c79127abe68e98e0e925b962e5
xinjinmsn <xinjin@msn.com>
2016-03-20 20:32:44 +0800

    Add the first controller BooksController

---
+++ ELibrary.WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using ELibrary.Data;
+using ELibrary.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace ELibrary.WebAPI.Controllers
+{
+    public class BooksController : ApiController
+    {
+        public IEnumerable<Book> Get()
+        {
+            var repo = new ELibraryRepository(new ELibraryContext());
+
+            var results = repo.GetAllBooks()
+                .OrderBy(f => f.Title)
+                .Take(10)
+                .ToList();
+
+            return results;
+        }
+    }
+}
