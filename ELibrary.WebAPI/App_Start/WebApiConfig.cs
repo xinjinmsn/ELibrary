6b522707e2078f32764cef5e719fd91def5a9d1a
xinjinmsn <xinjin@msn.com>
2016-03-20 16:05:13 +0800

    Add WebApiConfig class with Register method

---
+++ ELibrary.WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ELibrary.WebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+        }
+    }
+}
