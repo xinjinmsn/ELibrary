17c92ebbe1c265b21c4834791e3e6483c99f04d4
xinjinmsn <xinjin@msn.com>
2016-03-20 23:00:59 +0800

    Add CamelCasePropertyNamesContractResolver

--- ELibrary.WebAPI/App_Start/WebApiConfig.cs
+++ ELibrary.WebAPI/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,10 @@ namespace ELibrary.WebAPI
                 name:"DefaultApi",
                 routeTemplate:"api/{controller}/{id}",
                 defaults: new { id=RouteParameter.Optional});
+
+            var jsonFormatter = config.Formatters.JsonFormatter;
+
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
