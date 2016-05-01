c6d325ef6184f284049c5dd87eb2cf9d6ceb86c7
xinjinmsn <xinjin@msn.com>
2016-05-02 00:31:51 +0800

    Use Thread.CurrentPrincipal.Identity in ELibraryIdentityService to replace the hard-coded user name

--- ELibrary.WebAPI/Services/ELibraryIdentityService.cs
+++ ELibrary.WebAPI/Services/ELibraryIdentityService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 
 namespace ELibrary.WebAPI.Services
@@ -11,7 +12,7 @@ namespace ELibrary.WebAPI.Services
         {
             get
             {
-                return "testuser";
+                return Thread.CurrentPrincipal.Identity.Name;
             }
         }
     }
